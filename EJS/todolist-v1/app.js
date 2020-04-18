81bcd4507e19c62c6c6be7ad5f2d020abc77ae95
Mustafa Demir <mustafa@deplike.com>
2020-04-19 01:47:25 +0300

    Add work list. Add about us page. Implement template structure. Lecture 282

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -6,7 +6,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
+
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get('/', (req, res) => {
     const today = new Date();
@@ -20,17 +22,37 @@ app.get('/', (req, res) => {
     const day = today.toLocaleDateString('en-US', options);
 
     res.render('list', {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: items
     });
 
 });
 
 app.post('/', (req, res) => {
-    item = req.body.newItem;
-    items.push(item);
-    res.redirect('/');
+    let item = req.body.newItem;
+
+    if (req.body.list === 'Work') {
+        workItems.push(item);
+        res.redirect('/work');
+    } else {
+        items.push(item);
+        res.redirect('/');
+    }
 });
+
+app.get('/work', (req, res) => {
+    res.render('list', { listTitle: 'Work List', newListItems: workItems })
+});
+
+app.post('/work', (req, res) => {
+    const item = req.body.newItem;
+    workItems.push(item);
+    res.redirect('/work')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
